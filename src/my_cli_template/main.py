01305c0891d9ca2ea6b1aca8abca276a998bd271
Khalil Saboor <39493781+TheCloutEngineer@users.noreply.github.com>
2025-12-29 15:39:35 -0500

    add utils module to retrieve numpy version and implement main entry point

---
+++ src/my_cli_template/main.py
@@ -0,0 +1,9 @@
+from my_cli_template import utils
+
+
+def main() -> None:
+    """
+    Main entry point for my-cli-template.
+    """
+    print("Hello from my-cli-template!")
+    print(f"Numpy version: {utils.numpy_version()}")
---
+++ src/my_cli_template/utils.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def numpy_version():
+    """
+    Returns the version of the installed numpy package.
+    :return: str - Numpy version
+    """
+    return np.__version__
